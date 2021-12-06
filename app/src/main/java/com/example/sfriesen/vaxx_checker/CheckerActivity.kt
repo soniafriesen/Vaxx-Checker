ed9a61618bff38e14a6f833c1ea6c51eed015532
rdcamaral5 <78941553+rdcamaral5@users.noreply.github.com>
2021-12-06 04:59:32 -0500

    small start

---
+++ app/src/main/java/com/example/sfriesen/vaxx_checker/CheckerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sfriesen.vaxx_checker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checker)
+    }
+}
